93bc8f3744c9f342edf3fb0c4239224dd6244a2d
zyec32 <njkmrjpfzw@mail.ru>
2021-12-08 11:08:51 +0300

    fix: warns unusing impirts

--- src/components/ItemDapplet/index.tsx
+++ src/components/ItemDapplet/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import cn from 'classnames';
-import { Button, Checkbox, Image } from 'semantic-ui-react';
+import { Checkbox, Image } from 'semantic-ui-react';
 import { ethers } from 'ethers';
 import styled from 'styled-components';
 
